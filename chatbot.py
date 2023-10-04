34d37bac9e5c1ab7cd01942a0eb26dfdae8d96d4
wizardbck <146907875+wizardbck@users.noreply.github.com>
2023-10-04 12:35:41 +0900

    Update chatbot.py
    st.markdown to st.write

--- chatbot.py
+++ chatbot.py
@@ -13,13 +13,13 @@ if "messages" not in st.session_state:
 # Display chat messages from history on app rerun
 for message in st.session_state.messages:
   with st.chat_message(message["role"]):
-    st.markdown(message["content"])
+    st.write(message["content"])
 
 # React to user input
 if prompt := st.chat_input("What is up?"):
   # Display user message in chat message container
   with st.chat_message("user"):
-    st.markdown(prompt)
+    st.write(prompt)
   # Add user message to chat history
   st.session_state.messages.append({"role": "user", "content": prompt})
 
@@ -30,6 +30,6 @@ if prompt := st.chat_input("What is up?"):
 
   # Display assistant response in chat message container
   with st.chat_message("assistant"):
-    st.markdown(response)
+    st.write(response)
   # Add assistant response to chat history
   st.session_state.messages.append({"role": "assistant", "content": response})
